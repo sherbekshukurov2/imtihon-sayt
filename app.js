80251534306efeed481fb0855daa649c46399064
sherbekshukurov2 <146548826+sherbekshukurov2@users.noreply.github.com>
2023-10-02 16:53:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+  
+  let text = document.querySelector('.text-big2');
+
+  text.textContent = 'The Service We Provide For You'
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
